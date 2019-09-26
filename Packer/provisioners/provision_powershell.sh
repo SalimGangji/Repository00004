a702c878b8d275f81ec0e76f881e4fadd2c3936d
Salim Gangji <salim.gangji@gmail.com>
2019-09-26 11:17:55 -0700

    updating_customized_ops_image_uploading_new_centos_image

---
+++ Packer/provisioners/provision_powershell.sh
@@ -0,0 +1,7 @@
+### powershell ###
+sleep 30
+# Register the Microsoft RedHat repository
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+
+# Install PowerShell
+sudo yum install --assumeyes powershell
