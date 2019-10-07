29a58207ef88a0b7e30f6f060f28d22c38722e6f
SalimGangji <salim.gangji@gmail.com>
2019-10-07 12:32:01 -0700

    Update Microsoft.PowerShell_profile.ps1

--- PowerShellCore/Microsoft.PowerShell_profile.ps1
+++ PowerShellCore/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,18 @@
 new-alias -name "ll" -value "get-childitem"
 
+#Windows
 set-psreadlineoption -Colors @{"Command"=[ConsoleColor]::Yellow}
+
+#Linux
+set-psreadlineoption -Colors @{"Command"=[ConsoleColor]::DarkCyan}
+
 set-psreadlineoption -Colors @{"Member"=[ConsoleColor]::Blue}
 set-psreadlineoption -Colors @{"Number"=[ConsoleColor]::DarkBlue}
 set-psreadlineoption -Colors @{"Parameter"=[ConsoleColor]::Gray}
 set-psreadlineoption -Colors @{"Type"=[ConsoleColor]::DarkMagenta}
 set-psreadlineoption -Colors @{"Comment"=[ConsoleColor]::Blue}
 set-psreadlineoption -Colors @{"String"=[ConsoleColor]::Cyan}
-set-psreadlineoption
+get-psreadlineoption
 
+#Windows
 show-tmoutputcolor
