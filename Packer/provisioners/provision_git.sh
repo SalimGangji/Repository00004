3eaeef466f1a35f4ed3b545bf0cbe041a3eeb017
Salim Gangji <salim.gangji@gmail.com>
2019-11-07 03:50:01 +0000

    updated provision_git.sh

--- Packer/provisioners/provision_git.sh
+++ Packer/provisioners/provision_git.sh
@@ -16,8 +16,8 @@ declare -a git_prereq_packages=(
 
 sudo yum --assumeyes install ${git_prereq_packages[@]}
 
-sudo curl --location https://github.com/git/git/archive/v2.22.0.tar.gz --output /var/tmp/git_v2.22.0.tar.gz
+sudo curl --location https://github.com/git/git/archive/v2.24.0.tar.gz --output /var/tmp/git_v2.24.0.tar.gz
 sudo mkdir --parents /var/tmp/git_compile_from_source
-sudo tar --extract --verbose --file /var/tmp/git_v2.22.0.tar.gz --directory=/var/tmp/git_compile_from_source
-cd /var/tmp/git_compile_from_source/git-2.22.0/
+sudo tar --extract --verbose --file /var/tmp/git_v2.24.0.tar.gz --directory=/var/tmp/git_compile_from_source
+cd /var/tmp/git_compile_from_source/git-2.24.0/
 sudo make prefix=/usr install install-doc install-html
