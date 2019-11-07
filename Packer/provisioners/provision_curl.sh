028ccce0ba004e490ed88c8493b17a3df0412e38
Salim Gangji <salim.gangji@gmail.com>
2019-11-07 03:18:22 +0000

    updated provision_curl.sh

--- Packer/provisioners/provision_curl.sh
+++ Packer/provisioners/provision_curl.sh
@@ -1,10 +1,10 @@
 ### curl ###
-sleep 30
+sleep 3
 
-sudo curl --location https://github.com/curl/curl/releases/download/curl-7_65_3/curl-7.65.3.tar.gz --output /var/tmp/curl-7.65.3.tar.gz
+sudo curl --location https://github.com/curl/curl/releases/download/curl-7_67_0/curl-7.67.0.tar.gz --output /var/tmp/curl-7.67.0.tar.gz
 sudo mkdir --parents /var/tmp/curl_compile_from_source
-sudo tar --extract --verbose --file /var/tmp/curl-7.65.3.tar.gz --directory=/var/tmp/curl_compile_from_source
-cd /var/tmp/curl_compile_from_source/curl-7.65.3/
+sudo tar --extract --verbose --file /var/tmp/curl-7.67.0.tar.gz --directory=/var/tmp/curl_compile_from_source
+cd /var/tmp/curl_compile_from_source/curl-7.67.0/
 sudo ./configure
 sudo make
 sudo make install
