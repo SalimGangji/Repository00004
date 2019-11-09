c75cce6ff4cecdb2ca78f299aa8d1887413f1d38
SalimGangji <salim.gangji@gmail.com>
2019-11-08 17:24:43 -0800

    Update provision_git.sh

--- Packer/provisioners/provision_git.sh
+++ Packer/provisioners/provision_git.sh
@@ -20,4 +20,9 @@ sudo curl --location https://github.com/git/git/archive/v2.24.0.tar.gz --output 
 sudo mkdir --parents /var/tmp/git_compile_from_source
 sudo tar --extract --verbose --file /var/tmp/git_v2.24.0.tar.gz --directory=/var/tmp/git_compile_from_source
 cd /var/tmp/git_compile_from_source/git-2.24.0/
+
+# for some reason on AWS amazon-linux-2 instances these packages do not install correctly, so installing them again here
+sleep 30
+sudo yum install python3 libcurl-devel expat-devel asciidoc xmlto --assumeyes
+
 sudo make prefix=/usr install install-doc install-html
