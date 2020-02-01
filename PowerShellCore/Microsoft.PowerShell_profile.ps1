6863b37d53f95009e865207f7dd873c9dd41a191
Salim Gangji <salim.gangji@gmail.com>
2020-01-31 22:09:15 -0800

    updated comments

--- PowerShellCore/Microsoft.PowerShell_profile.ps1
+++ PowerShellCore/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,21 @@ new-alias -name "ll" -value "get-childitem"
 #[System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Windows)
 
 #https://docs.microsoft.com/en-us/powershell/module/psreadline/set-psreadlineoption?view=powershell-7
+# ContinuationPrompt: The color of the continuation prompt.
+# Emphasis: The emphasis color. For example, the matching text when searching history.
+# Error: The error color. For example, in the prompt.
+# Selection: The color to highlight the menu selection or selected text.
+# Default: The default token color.
+# Comment: The comment token color.
+# Keyword: The keyword token color.
+# String: The string token color.
+# Operator: The operator token color.
+# Variable: The variable token color.
+# Command: The command token color.
+# Parameter: The parameter token color.
+# Type: The type token color.
+# Number: The number token color.
+# Member: The member name token color.
 
 if($IsWindows){
     #Windows
