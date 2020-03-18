8498a1e39447c1ffc3adb3745212698b702044ff
SalimGangji <salim.gangji@gmail.com>
2020-03-18 13:45:25 -0700

    Update provision_docker.sh

--- Packer/provisioners/provision_docker.sh
+++ Packer/provisioners/provision_docker.sh
@@ -7,26 +7,27 @@ sudo yum install --assumeyes yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install --assumeyes docker-ce docker-ce-cli containerd.io
 
+## Steps removed because they do not seem to be necessary
 ## Create /etc/docker directory
-sudo mkdir --parents /etc/docker
+#sudo mkdir --parents /etc/docker
 
 ## Setup daemon
-sudo bash -c "cat >> /etc/docker/daemon.json" << EOL
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2",
-  "storage-opts": [
-    "overlay2.override_kernel_check=true"
-  ]
-}
-EOL
+#sudo bash -c "cat >> /etc/docker/daemon.json" << EOL
+#{
+#  "exec-opts": ["native.cgroupdriver=systemd"],
+#  "log-driver": "json-file",
+#  "log-opts": {
+#    "max-size": "100m"
+#  },
+#  "storage-driver": "overlay2",
+#  "storage-opts": [
+#    "overlay2.override_kernel_check=true"
+#  ]
+#}
+#EOL
 
 # create place where docker service can be configured
-sudo mkdir --parents /etc/systemd/system/docker.service.d
+#sudo mkdir --parents /etc/systemd/system/docker.service.d
 
 # Restart docker
 sudo systemctl daemon-reload
