ddd9e2801ce94477dce5c29e97ffc43d0d3f53dd
SalimGangji <salim.gangji@gmail.com>
2020-03-19 12:57:53 -0700

    Update provision_git.sh

--- Packer/provisioners/provision_git.sh
+++ Packer/provisioners/provision_git.sh
@@ -26,7 +26,6 @@ cd /var/tmp/git_compile_from_source/git-${git_version}/
 # for some reason on AWS amazon-linux-2 instances these packages do not install correctly, so installing them again here
 sleep 30
 sudo yum install python3 libcurl-devel expat-devel asciidoc xmlto --assumeyes
-
 sudo make prefix=/usr install install-doc install-html
 
 rm -rf /var/tmp/git_compile_from_source
