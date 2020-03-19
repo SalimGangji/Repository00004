9b7df5feb1deb6b2827fb8bdbc4fce237f2be77c
SalimGangji <salim.gangji@gmail.com>
2020-03-19 12:47:18 -0700

    Update provision_curl.sh

--- Packer/provisioners/provision_curl.sh
+++ Packer/provisioners/provision_curl.sh
@@ -1,11 +1,15 @@
 ### curl ###
 sleep 30
 
-sudo curl --location https://github.com/curl/curl/releases/download/curl-7_67_0/curl-7.67.0.tar.gz --output /var/tmp/curl-7.67.0.tar.gz
+curl_version="7.69.1"
+curl_version_underscore=$( echo $curl_version | tr "." "_" )
+
+sudo curl --location https://github.com/curl/curl/releases/download/curl-${curl_version_underscore}/curl-${curl_version}.tar.gz --output /var/tmp/curl-${curl_version}.tar.gz
 sudo mkdir --parents /var/tmp/curl_compile_from_source
-sudo tar --extract --verbose --file /var/tmp/curl-7.67.0.tar.gz --directory=/var/tmp/curl_compile_from_source
-cd /var/tmp/curl_compile_from_source/curl-7.67.0/
+sudo tar --extract --verbose --file /var/tmp/curl-${curl_version}.tar.gz --directory=/var/tmp/curl_compile_from_source
+cd /var/tmp/curl_compile_from_source/curl-${curl_version}/
 sudo ./configure
 sudo make
 sudo make install
 
+rm -rf /var/tmp/curl_compile_from_source
